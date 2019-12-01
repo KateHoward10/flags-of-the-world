a11964bc7c7662eaab82160a371c883bc00d6cfc
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-01 22:10:22 +0000

    Translate logic into React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { sendNameToServer } from './socket';
 
 function App({ players, dispatch }) {
   const [joined, setJoined] = useState(false);
   const [name, setName] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [question, setQuestion] = useState('');
+  const [options, setOptions] = useState([]);
+  const [rightAnswer, setRightAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [questionsAsked, setQuestionsAsked] = useState(0);
   const inCharge = players.length && players[0].name === name;
+  const endpoint = 'https://restcountries.eu/rest/v2/all';
+  const questionTypes = ['capital', 'name', 'alpha2Code', 'flag'];
 
   function submitName(e) {
     e.preventDefault();
@@ -13,6 +21,48 @@ function App({ players, dispatch }) {
     sendNameToServer({ name });
     setJoined(true);
   }
+
+  function generateQuestion() {
+    const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+    const questionType = questionTypes[Math.floor(Math.random() * questionTypes.length)];
+    setQuestion(
+      `${
+        questionType === 'capital'
+          ? `What is the capital of ${randomCountry.name}`
+          : questionType === 'name'
+          ? `Which country's capital is ${randomCountry.capital}`
+          : questionType === 'flag'
+          ? 'Which country’s flag is this'
+          : `What is the flag of ${randomCountry.name}`
+      }?`
+    );
+    const property = questionType === 'flag' ? 'name' : questionType;
+    setRightAnswer(randomCountry[property]);
+    let optionsToSet = [randomCountry[property]];
+    for (let i = 0; i < 3; i++) {
+      const filteredCountries = countries.filter(country => !options.includes(country[property]));
+      optionsToSet.push(filteredCountries[Math.floor(Math.random() * filteredCountries.length)][property]);
+    }
+    setOptions(optionsToSet.sort(() => Math.random() - 0.5));
+  }
+
+  function checkGuess(e) {
+    if (e.target.value === rightAnswer) {
+      setScore(score + 1);
+    }
+    setQuestionsAsked(questionsAsked + 1);
+  }
+
+  useEffect(() => {
+    if (inCharge && !countries.length) {
+      fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data =>
+          setCountries([...data.filter(country => country.name.length && country.capital.length && country.alpha2Code)])
+        );
+    }
+    if (countries.length) generateQuestion();
+  }, [inCharge, countries]);
 
   return (
     <div>
@@ -36,6 +86,17 @@ function App({ players, dispatch }) {
           </form>
         )}
       </div>
+      <p>{question}</p>
+      <div>
+        {options.map((option, index) => (
+          <button key={index} value={option} onClick={checkGuess}>
+            {option}
+          </button>
+        ))}
+      </div>
+      <p>
+        Score: {score} / {questionsAsked}
+      </p>
     </div>
   );
 }
