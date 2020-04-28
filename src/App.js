b923c81811a6539ec812693a55bc107ae79644ce
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-28 15:41:44 +0100

    Add flags to first screen

--- src/App.js
+++ src/App.js
@@ -227,6 +227,11 @@ function App({ dispatch, players, question, numberOfQuestions, questionsAsked })
       ) : (
         <div className="container">
           <h1>Welcome to Flags of the World</h1>
+          <img src="https://www.countryflags.io/mz/flat/64.png" alt="Flag of Mozambique" />
+          <img src="https://www.countryflags.io/np/flat/64.png" alt="Flag of Nepal" />
+          <img src="https://www.countryflags.io/va/flat/64.png" alt="Flag of Vatican City" />
+          <img src="https://www.countryflags.io/sc/flat/64.png" alt="Flag of Seychelles" />
+          <p><small>Be warned, there are questions about capitals too.</small></p>
           <p>Please enter a username to begin</p>
           <form onSubmit={submitName}>
             <input onChange={e => setName(e.target.value)} placeholder="Username" autoFocus />
